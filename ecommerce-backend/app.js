051bcce7fb8d1c3bded6b930033a08c23c3b473c
Hayat <hayat11.mf@gmail.com>
2026-02-10 22:26:28 -0800

    Completed product CRUD

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -5,7 +5,8 @@ const app = express();
 
 
 app.use(express.json()); 
-
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Server is running!' });
---
+++ ecommerce-backend/controllers/productController.js
@@ -0,0 +1,77 @@
+const Product = require('../models/product');
+
+// @desc    Get all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Public
+const createProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Public
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    product.name = req.body.name || product.name;
+    product.price = req.body.price || product.price;
+    product.description = req.body.description || product.description;
+    product.category = req.body.category || product.category;
+    product.countInStock = req.body.countInStock || product.countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Public
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    await product.deleteOne();
+    res.json({ message: 'Product removed' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
